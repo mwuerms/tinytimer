3b0c0094fb4884275674437fd3931e437c13fee0
Martin Egli <m_wuerms@gmx.net>
2023-03-21 20:06:25 +0100

    timytimer started project

---
+++ microchip-studio/tinytimer/main.c
@@ -0,0 +1,18 @@
+/*
+ * tinytimer.c
+ *
+ * Created: 21.03.2023 19:57:59
+ * Author : Martin
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
